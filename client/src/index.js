740c0a051084424436db3f2b36847428f54760d2
VirtuaBoza <andrew.m.boza@gmail.com>
2020-02-01 10:39:33 -0500

    Adds firebase for auth

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,29 @@
+import * as firebase from 'firebase';
+// import * as firebaseui from 'firebaseui';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPbdFzMe8efnyAmzIK8Sj0vi2LXUtXvpg',
+  authDomain: 'gravaddress-consumer-app.firebaseapp.com',
+  databaseURL: 'https://gravaddress-consumer-app.firebaseio.com',
+  projectId: 'gravaddress-consumer-app',
+  storageBucket: 'gravaddress-consumer-app.appspot.com',
+  messagingSenderId: '343734860363',
+  appId: '1:343734860363:web:b4781bc6769f554f5cebce',
+  measurementId: 'G-06CLBWYYZ1'
+};
+
+firebase.initializeApp(firebaseConfig);
+// const ui = new firebaseui.auth.AuthUI(firebase.auth());
+// ui.start('#root', {
+//   signInOptions: [
+//     firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//     firebase.auth.GoogleAuthProvider.PROVIDER_ID
+//   ]
+// });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
